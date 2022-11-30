f293bebfcf95610aab820cc3340c374faa9ba47b
viciscat <51047087+viciscat@users.noreply.github.com>
2022-11-30 08:51:34 +0100

    class Tache pour Katia

--- main.py
+++ main.py
@@ -8,26 +8,36 @@ gérant une liste de tâches à faire
 from flask import *
 import bdd
 
+
 class Tache:
-    def __init__(self):
+    def __init__(self, idTache, nom, description, idCategorie, idEtat, idPriorite, dateLimite, dateFin):
         # TODO: Faire la class! Si pas d'idée de méthodes, juste faire un dico
-        pass
+        self.idTache = idTache
+        self.nom = nom
+        self.description = description
+        self.idCategorie = idCategorie
+        self.idEtat = idEtat
+        self.idPriorite = idPriorite
+        self.dateLimite = dateLimite
+        self.dateFin = dateFin
+
 
 # Création des objets Flask et Bdd
 app = Flask(__name__)
 database = bdd.Bdd()
 
-taches = []
-
+taches = [Tache(*i) for i in database.getTaches()]
 
 
 # Les routes associées aux fonctions
 @app.route("/")
 def accueillir():
     """Gère l'accueil des utilisateurs"""
+    print(taches)
 
     # Rendu de la vue
     return render_template("accueil.html")
+
 
 # TODO : ajoutez de nouvelles routes associées à des fonctions "contrôleur" Python
 
@@ -35,3 +45,4 @@ def accueillir():
 # Lancement du serveur
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=1664, threaded=True, debug=True)
+
